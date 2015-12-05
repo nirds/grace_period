be28104eab817bfcaf0bbd065e3c996561030dfe
Whitney Levis <ms.whitney.levis@gmail.com>
2015-12-04 16:02:14 -0800

    wrap adding files functionality in a class and method.

--- lib/grace_period.rb
+++ lib/grace_period.rb
@@ -2,6 +2,15 @@ require "grace_period/version"
 require 'contracts'
 
 module GracePeriod
+  class Installer
+    def install
+      Dir.mkdir("grace_periods")
+      File.open("grace_periods/expiration_example.yml", "w") do |file|
+
+      file << "This is going to the output file"
+      end
+    end
+  end
 
   class ContractClient
     include Contracts::Core
--- lib/grace_period/gp_install.rb
+++
@@ -1,6 +0,0 @@
-Dir.mkdir("grace_periods")
-File.new("grace_periods/expiration_example.yml", "w") do |file|
-
-  file << "This is going to the output file"
-
-end
