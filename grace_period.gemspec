5ae7f363edf6d6f7cd1f9eb78480f9e6d526ae78
Mike Pence <mike.pence@gmail.com>
2015-12-11 11:30:15 -0500

    Removing Contracts.ruby

--- grace_period.gemspec
+++ grace_period.gemspec
@@ -22,8 +22,6 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency 'contracts', '~> 0.12.0'
-
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- spec/grace_period_spec.rb
+++ spec/grace_period_spec.rb
@@ -8,25 +8,4 @@ describe GracePeriod do
   it 'does something useful' do
     expect(true).to eq(true)
   end
-
-  it 'does bad stuff with Contracts' do
-    client = GracePeriod::ContractClient.new
-    expect { client.double('oops') }.to raise_error(ParamContractError)
-  end
-
-  it 'does good stuff with Contracts' do
-    client = GracePeriod::ContractClient.new
-    expect { client.double(1) }.to_not raise_error
-  end
-
-  it 'raises an error when time is in the past' do
-    client = GracePeriod::ContractClient.new
-    expect { client.future(Time.now.to_i - 1000000)}.to raise_error(ReturnContractError)
-  end
-
-  it "doesn't raise an error when time is in the future" do
-   client = GracePeriod::ContractClient.new
-   expect { client.future(Time.now.to_i + 1000000)}.to_not raise_error
-  end
-
 end
